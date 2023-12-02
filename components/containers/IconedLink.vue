641772df8bda38b7adbe3cc93dff6531168bc613
Innosan <reau.beua@gmail.com>
2023-12-02 03:02:44 +0300

    static bottom footer fix

---
+++ components/containers/IconedLink.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = defineProps({
+	navigateTo: String,
+	icon: String,
+});
+</script>
+
+<template>
+	<div class="flex gap-3">
+		<img :src="'~assets/icons/ui/'" alt="" />
+		<a class="general-link" href="https://goalgo.ru/" target="_blank"
+			>Страница хакатона</a
+		>
+	</div>
+</template>
+
+<style scoped></style>
