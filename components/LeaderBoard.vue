07fb164297582d4d771233f7ac2337ea28f6c64e
Innosan <reau.beua@gmail.com>
2023-12-13 09:44:50 +0300

    fixed signals

--- components/LeaderBoard.vue
+++ components/LeaderBoard.vue
@@ -7,19 +7,31 @@ const securitiesStore = useSecuritiesStore();
 		<p class="text-2xl font-black">Высокий потенциал роста</p>
 		<Tabs as="div" first-tab-title="Сегодня" second-tab-title="На 5 дней">
 			<template #tab1>
-				<div class="flex justify-between gap-4 flex-wrap cards">
+				<div
+					v-if="securitiesStore.dayLeaderboard.length !== 0"
+					class="flex justify-between gap-4 flex-wrap cards"
+				>
 					<LeaderCard
 						v-for="leader in securitiesStore.getDaySortedLeaderboard()"
 						:leader="leader"
 					/>
 				</div>
+				<div v-else class="opacity-70 font-bold">
+					<p>Пока данных нет.</p>
+				</div>
 			</template>
 			<template #tab2>
-				<div class="flex justify-between gap-4 flex-wrap cards">
+				<div
+					v-if="securitiesStore.fiveDayLeaderboard.length !== 0"
+					class="flex justify-between gap-4 flex-wrap cards"
+				>
 					<LeaderCard
 						v-for="leader in securitiesStore.getFiveDaySortedLeaderboard()"
 						:leader="leader"
 					/>
+				</div>
+				<div v-else class="opacity-70 font-bold">
+					<p>Пока данных нет.</p>
 				</div>
 			</template>
 		</Tabs>
--- pages/service/signals/all.vue
+++ pages/service/signals/all.vue
@@ -14,17 +14,59 @@ const signals = computed(() =>
 	</div>
 	<div v-else class="flex gap-7 flex-wrap">
 		<div
-			v-for="signals in signals"
+			v-for="signal in signals"
 			class="card hover:border-b-red-400 transition-all flex gap-4 flex-col p-4 border-2 rounded-xl"
 		>
-			<CardHeading
-				:heading="'Сигнал ' + signals.id"
-				icon="ui/ic_signal"
-			/>
-			<CardHeading
-				:heading="statuses[signals.status].title"
-				:icon="statuses[signals.status].icon"
-			/>
+			<div class="flex gap-4">
+				<div class="border-r-2 pr-4 gap-3 flex flex-col">
+					<CardHeading
+						:heading="'Сигнал ' + signal.id"
+						icon="ui/ic_signals"
+					/>
+					<CardHeading
+						:heading="statuses[signal.status].title"
+						:icon="statuses[signal.status].icon"
+					/>
+				</div>
+				<div class="flex flex-col gap-6">
+					<div class="flex gap-4">
+						<div>
+							<Tooltip :description="timeframes.description">
+								<p class="font-bold">Таймфрейм</p>
+								<p>{{ signal.config.timeframe }}</p>
+							</Tooltip>
+						</div>
+						<div>
+							<Tooltip :description="maxBars.description">
+								<p class="font-bold">Максимум неотмечено</p>
+								<p>{{ signal.config.max_unmark * 100 }} %</p>
+							</Tooltip>
+						</div>
+						<div>
+							<Tooltip :description="bars.description">
+								<p class="font-bold">Период разметки</p>
+								<p>{{ signal.config.extr_bar_count }}</p>
+							</Tooltip>
+						</div>
+					</div>
+					<div class="flex gap-4">
+						<div>
+							<Tooltip :description="countDays.description">
+								<p class="font-bold">Количество дней</p>
+								<p>{{ signal.config.count_days }}</p>
+							</Tooltip>
+						</div>
+						<div>
+							<Tooltip
+								:description="markupParameters.description"
+							>
+								<p class="font-bold">Параметр разметки</p>
+								<p>{{ signal.config.count_points }}</p>
+							</Tooltip>
+						</div>
+					</div>
+				</div>
+			</div>
 			<CloseTaskButton
 				v-if="signal.status !== 2 && signal.status !== 3"
 				:task-id="signal.id"
